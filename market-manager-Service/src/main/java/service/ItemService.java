677ce8b882abf9095e68a1deb82b5cd0ecfe6525
刘橙杭 <liuchenghang@tansun.com.cn>
2018-01-10 17:52:46 +0800

    导入

---
+++ market-manager-Service/src/main/java/service/ItemService.java
@@ -0,0 +1,7 @@
+package service;
+
+import com.pojo.TbItem;
+
+public interface  ItemService {
+	TbItem getItemById(long itemId);
+}
---
+++ market-manager-Service/src/main/java/service/imp/ItemServiceImp.java
@@ -0,0 +1,44 @@
+package service.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mapper.TbItemMapper;
+import com.pojo.TbItem;
+import com.pojo.TbItemExample;
+
+import service.ItemService;
+
+public class ItemServiceImp implements ItemService {
+	
+	private TbItemMapper tbItemMapper;
+
+	public TbItemMapper getTbItemMapper() {
+		return tbItemMapper;
+	}
+
+	public void setTbItemMapper(TbItemMapper tbItemMapper) {
+		this.tbItemMapper = tbItemMapper;
+	}
+
+	
+	@Override
+	public TbItem getItemById(long itemId) {
+		
+		
+		//TbItem item = itemMapper.selectByPrimaryKey(itemId);
+		//添加查询条件
+		TbItemExample example = new TbItemExample();
+//		Criteria criteria = example.createCriteria();
+//		criteria.andIdEqualTo(itemId);
+		List<TbItem> list = tbItemMapper.selectByExample(example);
+		if (list != null && list.size() > 0) {
+			TbItem item = list.get(0);
+			return item;
+		}
+		return null;
+		}
+
+}
